f3400c800cb36f8cf511afa57568f2f8c470bca5
tinagrit <mu@tinagrit.com>
2025-04-19 12:01:59 -0700

    stop points extract

---
+++ python/stops.py
@@ -0,0 +1,75 @@
+# Processes stops.csv downloaded from TransLink's GTFS Static Data
+# https://www.translink.ca/about-us/doing-business-with-translink/app-developer-resources/gtfs/gtfs-data
+# Author: Mu Leelawat
+
+import geojson
+from enum import Enum
+
+class Properties(Enum):
+    LAT = 0
+    WHEELCHAIR = 1
+    CODE = 2
+    LON = 3
+    ID = 4
+    URL = 5
+    PARENT = 6
+    DESC = 7
+    NAME = 8
+    TYPE = 9
+    ZONE = 10
+
+filenames = {
+    "input filename": "./geojson/stops.csv",
+    "output 1 filename": "./geojson/trainPoints.csv",
+    "output 2 filename": "./geojson/busPoints.csv"
+}
+
+for name, path in filenames.items():
+    ask = input(f'Enter the {name} if you do not want to use "{path}": ')
+    if ask != "":
+        path = ask
+
+tl = []
+with open(filenames["input filename"],"r") as f:
+    tl = f.readlines()
+
+tl = tl[1:]
+
+trainStations = []
+busStops = []
+
+for line in tl:
+    stop = line.strip().split(",")
+
+    hasParent = bool(stop[Properties.PARENT.value] != "")
+    if hasParent:
+        continue
+
+    lat = stop[Properties.LAT.value]
+    lon = stop[Properties.LON.value]
+    coord = (float(lat), float(lon))
+    point = geojson.Point(coord)
+
+    name = stop[Properties.NAME.value]
+    isWheelchairAccessible = bool(stop[Properties.WHEELCHAIR.value] == "1")
+
+    feature = geojson.Feature(geometry=point, properties={
+        "name": name,
+        "wheelchair": isWheelchairAccessible
+    })
+
+    isTrainStation = bool(stop[Properties.TYPE.value] == "1")
+    if isTrainStation:
+        trainStations.append(feature)
+    else:
+        busStops.append(feature)
+
+
+collectionTrainStations = geojson.FeatureCollection(trainStations)
+collectionBusStops = geojson.FeatureCollection(busStops)
+
+with open(filenames["output 1 filename"], "w") as f:
+    geojson.dump(collectionTrainStations, f)
+
+with open(filenames["output 2 filename"], "w") as f:
+    geojson.dump(collectionBusStops, f)
